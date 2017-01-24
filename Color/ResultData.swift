da18fd589aebdecb71877170e4375145f005406e
danielwang5 <dwq@mit.edu>
2017-01-23 23:04:53 -0500

    UI more stuff

---
+++ Color/ResultData.swift
@@ -0,0 +1,14 @@
+//
+//  ResultData.swift
+//  Color
+//
+//  Created by James Wang on 1/23/17.
+//  Copyright © 2017 DanielW. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ResultData{
+    
+}
