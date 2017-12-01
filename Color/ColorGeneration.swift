d4ad844d7877e5361498c4d3f742ff87c07d5f51
danielwang5 <dwq@mit.edu>
2017-12-01 03:17:00 -0500

    rgb to cie-lab converter function

--- Color/ColorGeneration.swift
+++ Color/ColorGeneration.swift
@@ -77,3 +77,87 @@ extension Float {
         return Float(arc4random()) / arc4randomMax
     }
 }
+
+func rgbToLab(R:Double, G:Double, B:Double) -> [Double]{
+    
+    var r, g, b, X, Y, Z, xr, yr, zr:Double
+    
+    // D65/2°
+    let Xr:Double = 95.047;
+    let Yr:Double = 100.0;
+    let Zr:Double = 108.883;
+    
+    
+    // --------- RGB to XYZ ---------//
+    
+    r = R/255.0;
+    g = G/255.0;
+    b = B/255.0;
+    
+    if (r > 0.04045){
+        r = pow((r+0.055)/1.055,2.4);
+    }
+    else{
+        r = r/12.92
+    }
+    
+    if (g > 0.04045){
+        g = pow((g+0.055)/1.055,2.4);
+    }
+    else{
+        g = g/12.92
+    }
+    
+    if (b > 0.04045){
+        b = pow((b+0.055)/1.055,2.4)
+    }
+    else{
+        b = b/12.92
+    }
+    
+    r*=100;
+    g*=100;
+    b*=100;
+    
+    X =  0.4124*r + 0.3576*g + 0.1805*b;
+    Y =  0.2126*r + 0.7152*g + 0.0722*b;
+    Z =  0.0193*r + 0.1192*g + 0.9505*b;
+    
+    
+    // --------- XYZ to Lab --------- //
+    
+    xr = X/Xr;
+    yr = Y/Yr;
+    zr = Z/Zr;
+    
+    if ( xr > 0.008856 ){
+        xr =  pow(xr, 1/3.0)
+    }
+    else{
+        xr = ((7.787 * xr) + 16 / 116.0);
+    }
+    
+    if ( yr > 0.008856 ){
+        yr = pow(yr, 1/3.0);
+    }
+    else{
+        yr = ((7.787 * yr) + 16 / 116.0);
+    }
+    
+    if ( zr > 0.008856 ){
+        zr =  pow(zr, 1/3.0);
+    }
+    else{
+        zr = ((7.787 * zr) + 16 / 116.0);
+    }
+    
+    
+    var lab:[Double] = [0,0,0];
+    
+    lab[0] = (116*yr)-16;
+    lab[1] = 500*(xr-yr);
+    lab[2] = 200*(yr-zr);
+    
+    return lab;
+    
+}
